fe0c4d691769173b33f96abb770a92d834355125
Giwbert <pc@DESKTOP-6TVDA5Q>
2024-07-21 08:22:32 -0400

    Creating the Command and Query interfaces.

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -1,19 +1,18 @@
 ﻿using Bookify.Domain.Bookings;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Bookify.Application
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
-            services.AddMediatR(configuration =>
-            {
-                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
-            });
+namespace Bookify.Application;
 
-            services.AddTransient<PricingService>();
-            return services;
-        }
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        services.AddTransient<PricingService>();
+        return services;
     }
 }
