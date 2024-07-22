bea74625cc1a7493c32d6169323ebbf468d69e73
Giwbert <pc@DESKTOP-6TVDA5Q>
2024-07-22 08:23:20 -0400

    Creating the infrastructure project.

---
+++ Bookify.Infraestructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infraestructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, IDateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+
+
+        return services;
+    }
+}
---
+++ Bookify.Infraestructure/Email/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    Task IEmailService.SendAsync(Domain.Users.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
